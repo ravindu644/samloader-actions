da5b9dcf326ef983f10b50368ca9e4be53efa14f
Ravindu Deshan <droidcasts@protonmail.com>
2025-03-29 22:02:38 +0530

    worker script: re-wrote
    Signed-off-by: Ravindu Deshan <droidcasts@protonmail.com>

--- tools/worker.sh
+++ tools/worker.sh
@@ -1,104 +1,67 @@
 #!/bin/bash
 
-extract(){
-	cd "$WDIR/Downloads" # Change directory
-	echo -e "\n${MINT_GREEN}[+] Extracting the firmware Zip...${RESET}\n"
-	unzip firmware.zip && rm firmware.zip
+# Required image files
+REQUIRED_IMAGES=(
+    "boot.img"
+    "init_boot.img"
+    "recovery.img"
+    "vbmeta.img"
+    "dt.img"
+    "dtbo.img"
+    "dtb.img"
+    "vendor_boot.img"
+)
 
-	for file in AP*.tar.md5; do
-    	tar -xvf "$file" && rm *.md5
-	done
-
-	echo -e "\n${LIGHT_YELLOW}[i] Zip Extraction Completed..!${RESET}"
-
-	chk_lz4(){	    
-	    files=$(find . -name "*.lz4")
-	    if [ -n "$files" ]; then
-	        echo -e "${MINT_GREEN}[i] Decompressing LZ4 files...${RESET}\n"
-	        lz4 -m *.lz4 > /dev/null 2>&1
-			rm *.lz4 #cleaning
-	    fi
-	}
-
-	chk_lz4
-}
-
-is_dynamic(){
-	cd "$WDIR/Downloads" # Change directory
-	if [ -e super.img ]; then
-		PARTITION_SCHEME=1
-        	echo -e "${MINT_GREEN}[i] Dynamic Partition Device Detected..!${RESET}\n"		
-
-	elif [ -e system.img ] && [ -e vendor.img ]; then
-		PARTITION_SCHEME=2
-        	echo -e "${MINT_GREEN}[i] Non-Dynamic Partition Device Detected..!${RESET}"	
-
-    elif [ -e system.img ] || [ -e system.img.ext4 ] && [ ! -e vendor.img ] ; then
-    	is_legacy=1   	
-
-    else
-        echo -e "${RED}[x] An Internal Error occured..!${RESET}"
-        exit 1
+extract() {
+    cd "$WDIR/Downloads"
+    
+    echo -e "\n${MINT_GREEN}[+] Extracting the firmware Zip...${RESET}\n"
+    
+    unzip firmware.zip && rm firmware.zip
+    
+    for file in AP*.tar.md5; do
+        tar -xvf "$file" && rm *.md5
+    done
+    
+    echo -e "\n${LIGHT_YELLOW}[i] Zip Extraction Completed..!${RESET}"
+    
+    # Check and decompress LZ4 files if they exist
+    files=$(find . -name "*.lz4")
+    if [ -n "$files" ]; then
+        echo -e "${MINT_GREEN}[i] Decompressing LZ4 files...${RESET}\n"
+        lz4 -m *.lz4 > /dev/null 2>&1
+        rm *.lz4
     fi
 }
 
-stock_files(){
-	echo -e "${MINT_GREEN}[+] Copying the Required stock files for Magisk...\n${RESET}"
-
-	#init_boot and vendor_boot
-	cd "$WDIR/Downloads" #changed dir
-	if [ -e init_boot.img ]; then
-		cp init_boot.img "$WDIR/output/"
-	fi
-	if [ -e vendor_boot.img ]; then
-		cp vendor_boot.img "$WDIR/output/"
-	fi	
-
-	if [ "$is_legacy" == 1 ] && [ -e system.img ] || [ -e system.img.ext4 ]; then		
-		cd "$WDIR/Downloads" #changed dir
-		cp boot.img recovery.img "$WDIR/output/"
-		cd "$WDIR/output" #changed dir
-		tar -cvf "$BASE_TAR_NAME" *.img && rm *.img #cleaning
-
-	elif [ "$PARTITION_SCHEME" == 1 ]; then
-			cd "$WDIR/Downloads" #changed dir
-			cp boot.img vbmeta.img recovery.img dtbo.img "$WDIR/output/"
-			cd "$WDIR/output" #changed dir
-			tar -cvf "$BASE_TAR_NAME" *.img && rm *.img #cleaning
-		else
-			cd "$WDIR/Downloads" #changed dir
-			dt_check(){
-				if [ -e dt.img ]; then
-					is_dt=1
-				fi
-				if [ -e dtbo.img ]; then
-					is_dtbo=1
-				fi
-			}
-			dt_check
-			if [ "$is_dt" == 1 ] && [ $is_dtbo == 1 ]; then 
-				cp boot.img vbmeta.img recovery.img dtbo.img dt.img "$WDIR/output/"
-				cd "$WDIR/output" #changed dir
-				tar -cvf "$BASE_TAR_NAME" *.img && rm *.img #cleaning
-			elif [ ! "$is_dt" == 1 ] && [ $is_dtbo == 1 ]; then
-				cp boot.img vbmeta.img recovery.img dtbo.img "$WDIR/output/"
-				cd "$WDIR/output" #changed dir
-				tar -cvf "$BASE_TAR_NAME" *.img && rm *.img #cleaning
-			else
-			 	cp boot.img vbmeta.img recovery.img "$WDIR/output/"
-				cd "$WDIR/output" #changed dir
-				tar -cvf "$BASE_TAR_NAME" *.img && rm *.img #cleaning
-			fi
-
-	fi
-	cd "$WDIR/output" ; mkdir "${MODEL}" ; mv "${BASE_TAR_NAME}" "${MODEL}"
-	export BASE_TAR_NAME="${MODEL}-Magisk-files.tar"
-	tar -cvf "${BASE_TAR_NAME}" "${MODEL}"
-	xz -9 --threads=0 "${BASE_TAR_NAME}"
-	mv "${BASE_TAR_NAME}.xz" "$WDIR/Dist"
-	echo -e "\n${LIGHT_YELLOW}[i] Zip file created: ${BASE_TAR_NAME}.xz${RESET}\n"
+collect_and_package_files() {
+    echo -e "${MINT_GREEN}[+] Copying the Required stock files for Magisk...${RESET}\n"
+    
+    # Create output directory if it doesn't exist
+    mkdir -p "$WDIR/output"
+    
+    # Copy all existing required images to output directory
+    cd "$WDIR/Downloads"
+    for img in "${REQUIRED_IMAGES[@]}"; do
+        if [ -e "$img" ]; then
+            echo -e "${LIGHT_YELLOW}[i] Copying $img${RESET}"
+            cp "$img" "$WDIR/output/"
+        fi
+    done
+    
+    # Create the tar file with all image files
+    cd "$WDIR/output"
+    TAR_NAME="${MODEL}-Magisk-files.tar"
+    tar -cvf "$TAR_NAME" *.img && rm *.img
+    
+    # Create maximum compressed zip from the tar file
+    mkdir -p "$WDIR/Dist"
+    zip -9 "$WDIR/Dist/${TAR_NAME}.zip" "$TAR_NAME"
+    rm "$TAR_NAME"
+    
+    echo -e "\n${LIGHT_YELLOW}[i] Zip file created: ${TAR_NAME}.zip${RESET}\n"
 }
 
+# Main execution
 extract
-is_dynamic
-stock_files
+collect_and_package_files
